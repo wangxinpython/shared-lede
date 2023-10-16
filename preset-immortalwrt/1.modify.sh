00952db88f5c6429e08197a559c3bdb6d6dc25ea
wangxinpython <130729392+wangxinpython@users.noreply.github.com>
2023-10-16 23:00:30 +0800

    Update 1.modify.sh

--- preset-immortalwrt/1.modify.sh
+++ preset-immortalwrt/1.modify.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-
+for configFile in $(ls target/linux/ramips/mt7621/config*)
+do
+    echo -e "\nCONFIG_NETFILTER_NETLINK_GLUE_CT=y" >> $configFile
+done
 #修改登录IP
 #sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
