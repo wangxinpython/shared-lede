e34d3f814b1dc1cb1a2466fa1bbcde4765e91c2f
wangxinpython <130729392+wangxinpython@users.noreply.github.com>
2023-10-17 13:47:23 +0800

    Update 1.clone.sh

--- preset-lede/1.clone.sh
+++ preset-lede/1.clone.sh
@@ -5,11 +5,11 @@ git clone --depth 1 https://github.com/coolsnowwolf/lede.git && mv ./lede/* ./
 
 # 下载插件
 rm -rf package/lean/luci-theme-argon
-mkdir -p package/supply-packages && cd package/supply-packages 
+mkdir -p package/supply-packages && cd package/supply-packages
 git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
 git clone --depth 1 https://github.com/kenzok8/openwrt-packages.git
 git clone --depth 1 https://github.com/kenzok8/small.git
-git clone --depth 1 https://github.com/CHN-beta/rkp-ipid.git 
-git clone --depth 1 https://github.com/Zxilly/UA2F.git 
+git clone --depth 1 https://github.com/CHN-beta/rkp-ipid.git
+git clone --depth 1 https://github.com/Zxilly/UA2F.git
 git clone --depth 1 https://github.com/lucikap/luci-app-ua2f.git
